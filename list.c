899495c0a62d362c77416c84733949a8f53b086a
iwang2 <iwang2@stuy.edu>
2017-10-17 10:39:52 -0400

    wrote some more functions - untested

--- list.c
+++ list.c
@@ -5,24 +5,59 @@
 #include "list.h"
 
 // insert song_nodes in the front
-struct node * insert_front(struct node *n, char *song, char *artist);
+struct node * insert_front(struct node *n, char *s, char *a){
+  struct node * new;
+  new = (struct node *)malloc(sizeof(struct node));
 
-struct node * insert_ordered(struct node *n, char *song, char *artist);
+  new->song = s;
+  new->artist = a;
+  new->next = n;
+
+  return new;
+}
+
+struct node * insert_ordered(struct node *n, char *song, char *artist){
+  
+}
 
 // print entire list
-void print_list(struct node *head);
+void print_list(struct node *head){
+  while(head){
+    printf("%s, %s\n", head->artist, head->song);
+    head = head->next;
+  }
+}
 
 // find and return pointer to an individual song
-struct node * find_song(struct node *n, char *song);
+struct node * find_song(struct node *n, char *s){
+  while(n->song != s){
+    n = n->next;
+  }
+  return n;
+}
 
 // find and return pointer to first song of an artist
-struct node * find_artist(struct node *n, char *artist);
+struct node * find_artist(struct node *n, char *a){
+  while(n->artist != a){
+    n = n->next;
+  }
+  return n;
+}
 
 // pointer to random element in list
 struct node * random(struct node *head);
 
 // remove single node from the list
-struct node * remove(struct node *n, char *song);
+struct node * remove(struct node *n, char *song){
+}
 
 // free entire list
-struct node * free_list(struct node *n);
+struct node * free_list(struct node *n){
+  struct node * temp;
+  while(n){
+    temp = n->next;
+    free(n);
+    n = temp;
+  }
+  return temp;
+}
